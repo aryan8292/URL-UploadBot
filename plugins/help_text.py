273c68cf8537a79f0efb9b29fe7dadf4f95019af
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-09-09 19:32:14 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -18,7 +18,7 @@ Config.UPDATE_CHANNEL = Config.UPDATES_CHANNEL
 async def help_user(bot, update):
     if Config.JOIN_CHANNEL is not None:
         try:
-            user = await c.get_chat_member(Config.UPDATES_CHANNEL, m.chat.id)
+            user = await bot.get_chat_member(Config.UPDATES_CHANNEL, m.chat.id)
             if user.status == "kicked":
                 await c.send_message(
                     chat_id=m.chat.id,
@@ -28,7 +28,7 @@ async def help_user(bot, update):
                 )
                 return
         except UserNotParticipant:
-            await c.send_message(
+            await bot.send_message(
                 chat_id=m.chat.id,
                 text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
                 reply_markup=InlineKeyboardMarkup(
@@ -42,7 +42,7 @@ async def help_user(bot, update):
             )
             return
         except Exception:
-            await c.send_message(
+            await bot.send_message(
                 chat_id=m.chat.id,
                 text="Something went Wrong. Contact my [👥 Support Group](https://t.me/HxSupport).",
                 parse_mode="markdown",
